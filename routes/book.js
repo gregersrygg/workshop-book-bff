f4426eb428a82211dc16a3c02910cd35f4e317c8
figrrygg <gregers.gram.rygg@finn.no>
2016-03-08 15:49:05 +0100

    Implement ESI without middleware

--- routes/book.js
+++ routes/book.js
@@ -4,22 +4,38 @@ var goodGuy = require('good-guy-http')({
 });
 var jp = require('jsonpath');
 var router = new express.Router();
+var ESI = require('nodesi');
+var esi = new ESI();
 
 var BOOK_SERVICE_URL = 'https://book-catalog-proxy-5.herokuapp.com/book?isbn=';
 
+function pickRelevantBookData (data) {
+    const volume = jp.value(data, '$..volumeInfo');
+
+    return {
+        bookTitle: volume.title,
+        subtitle: volume.subtitle,
+        bookCover: jp.value(volume, '$..thumbnail')
+    };
+}
+
+function renderPage (app, pageData) {
+    return new Promise((resolve, reject) => {
+        app.render('book', pageData, (err, html) => {
+            if (err) { return void reject(err); }
+            resolve(html);
+        });
+    });
+}
+
 router.get('/:isbn', function (req, res, next) {
-    goodGuy(`${BOOK_SERVICE_URL}${req.params.isbn}`).then(response => {
-        const data = JSON.parse(response.body);
-        const volume = jp.value(data, '$..volumeInfo');
-
-        var pageData = {
-            bookTitle: volume.title,
-            subtitle: volume.subtitle,
-            bookCover: jp.value(volume, '$..thumbnail')
-        };
-
-        res.render('book', pageData);
-    }).catch(next);
+    goodGuy(`${BOOK_SERVICE_URL}${req.params.isbn}`)
+    .then(response => JSON.parse(response.body))
+    .then(pickRelevantBookData)
+    .then(renderPage.bind(null, req.app))
+    .then(html =>  esi.process(html))
+    .then(html => res.send(html))
+    .catch(next);
 });
 
 module.exports = router;
