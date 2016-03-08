0bfc7a08c2625dc249cbb99388614b6396abf1fc
figrrygg <gregers.gram.rygg@finn.no>
2016-03-08 15:21:45 +0100

    Use jsonpath to parse json response and add finn-js-code-style

--- routes/book.js
+++ routes/book.js
@@ -2,18 +2,20 @@ var express = require('express');
 var goodGuy = require('good-guy-http')({
     maxRetries: 3
 });
-var router = express.Router();
+var jp = require('jsonpath');
+var router = new express.Router();
 
 var BOOK_SERVICE_URL = 'https://book-catalog-proxy-5.herokuapp.com/book?isbn=';
 
-router.get('/:isbn', function(req, res, next) {
+router.get('/:isbn', function (req, res, next) {
     goodGuy(`${BOOK_SERVICE_URL}${req.params.isbn}`).then(response => {
-        var data = JSON.parse(response.body);
-        const volume = data.items[0].volumeInfo
+        const data = JSON.parse(response.body);
+        const volume = jp.value(data, '$..volumeInfo');
+
         var pageData = {
             bookTitle: volume.title,
             subtitle: volume.subtitle,
-            bookCover: volume.imageLinks.thumbnail
+            bookCover: jp.value(volume, '$..thumbnail')
         };
 
         res.render('book', pageData);
