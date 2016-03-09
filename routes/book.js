3eae91ed896269c240e76b4d2e2f91cc6b0f9ced
figrrygg <gregers.gram.rygg@finn.no>
2016-03-09 11:09:07 +0100

    Add request-id to a meta tag

--- routes/book.js
+++ routes/book.js
@@ -12,14 +12,13 @@ var esi = new ESI({
 var BOOK_SERVICE_URL = 'https://book-catalog-proxy-5.herokuapp.com/book?isbn=';
 var BOOK_COUNT_URL = process.env.BOOK_COUNT_URL || 'http://book-service-gregers.herokuapp.com/stock';
 
-function pickRelevantBookData (isbn, data) {
+function pickRelevantBookData (data) {
     const volume = jp.value(data, '$..volumeInfo');
 
     return {
         bookTitle: volume.title,
         subtitle: volume.subtitle,
-        bookCover: jp.value(volume, '$..thumbnail'),
-        stockCountURL: `${BOOK_COUNT_URL}/${isbn}`
+        bookCover: jp.value(volume, '$..thumbnail')
     };
 }
 
@@ -41,7 +40,8 @@ router.get('/:isbn', function (req, res, next) {
     const requestId = req.headers['x-request-id'];
     goodGuy(`${BOOK_SERVICE_URL}${isbn}`)
     .then(response => JSON.parse(response.body))
-    .then(partial(pickRelevantBookData, isbn))
+    .then(pickRelevantBookData)
+    .then(bookData => Object.assign({ stockCountURL: `${BOOK_COUNT_URL}/${isbn}`, requestId }, bookData))
     .then(partial(renderPage, req.app))
     .then(html => esi.process(html, { headers: { 'Accept': 'text/html', 'x-request-id': requestId } }))
     .then(html => res.send(html))
